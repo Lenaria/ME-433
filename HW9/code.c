fa6a2d206ff4fa387ce43e4679e7ed3e88350204
Prarthana Chakrabarti <prarthana.chakrabarti@gmail.com>
2024-04-28 02:25:28 -0500

    HW9

---
+++ HW9/code.c
@@ -0,0 +1,128 @@
+// based on adafruit and sparkfun libraries
+
+#include <stdio.h>
+#include <string.h> // for memset
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+
+/// \tag::uart_advanced[]
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY    UART_PARITY_NONE
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+static int chars_rxed = 0;
+
+volatile int i = 0, row = 0;
+char message[100];
+
+// RX interrupt handler
+void on_uart_rx() {
+    //what the zero sends back
+    
+    while (uart_is_readable(UART_ID)) {
+        uint8_t ch = uart_getc(UART_ID);
+        if(ch == '\n' || ch == '\r')
+        {
+            i = 0;
+            printf("Reflected by Zero: %s\n\n", message);
+        }
+        
+        else
+        {
+            message[i] = ch;
+            i++;
+        }
+    }
+}
+
+int main(){
+    
+    //printf("Start!\n");
+    stdio_init_all();
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+    printf("Start 2!\n");
+    
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // I2C is "open drain", pull ups to keep signal high when no data is being sent
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+    // Set up our UART with a basic baud rate.
+    uart_init(UART_ID, 2400);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Actually, we want a different speed
+    // The call will return the actual baud rate selected, which will be as close as
+    // possible to that requested
+    int __unused actual = uart_set_baudrate(UART_ID, BAUD_RATE);
+
+    // Set UART flow control CTS/RTS, we don't want these, so turn them off
+    uart_set_hw_flow(UART_ID, false, false);
+
+    // Set our data format
+    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+    // Turn off FIFO's - we want to do this character by character
+    uart_set_fifo_enabled(UART_ID, false);
+
+    // Set up a RX interrupt
+    // We need to set up the handler first
+    // Select correct interrupt for the UART we are using
+    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+
+    // And set up and enable the interrupt handlers
+    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+    irq_set_enabled(UART_IRQ, true);
+
+    // Now enable the UART to send interrupts - RX only
+    uart_set_irq_enables(UART_ID, true, false);
+
+    // OK, all set up.
+    // Lets send a basic string out, and then run a loop and wait for RX interrupts
+    // The handler will count them, but also reflect the incoming data back with a slight change!
+    uart_puts(UART_ID, "\nHello, uart interrupts\n");
+
+    int inp;
+    char inp_pico[100];    
+    
+    while(1){
+        //Heartbeat
+        //blink GP25
+        gpio_put(LED_PIN, 1);
+        sleep_ms(200);
+        //printf("Start!\n");
+
+        //Write to Zero:
+        scanf("%d", &inp);
+        printf("Sending %d to the Zero\n", inp);
+
+        sprintf(inp_pico, "%d\n", inp);
+        uart_puts(UART_ID, inp_pico);
+
+        gpio_put(LED_PIN, 0);
+        
+        sleep_ms(300);
+    }
+}
